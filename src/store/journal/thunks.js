f0f90320ddbdc7e604497486c03af883b54619e2
Terrasa <Sebastian.Terrasa@adesso.es>
2022-10-25 12:34:55 +0200

    images urls

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -16,6 +16,7 @@ export const startNewNote = () => {
             title: '',
             body: '',
             date: new Date().getTime(),
+            imageUrls: []
         }
 
         const newDoc = doc( collection( FirebaseDB, `${ uid }/journal/notes`) );
